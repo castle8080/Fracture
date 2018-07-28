bf2b53bed96732b5ae97822d28e0c3407bf1fe47
Bryan Castillo <castillo.bryan@gmail.com>
2018-07-27 20:27:40 -0700

    Starting some code.

---
+++ Fracture/Fracture/Controllers/FractalImageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fracture.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/FractalImage")]
+    public class FractalImageController : Controller
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+
+            return "foo";
+        }
+
+    }
+}
---
+++ Fracture/Fracture/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fracture.Controllers
+{
+    [Produces("text/html")]
+    public class HomeController : Controller
+    {
+        [Route("index.html"), Route("")]
+        public ContentResult Index()
+        {
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                StatusCode = (int)HttpStatusCode.Accepted,
+                Content =
+                    "<!DOCTYPE html>\n" +
+                    "<html>\n" +
+                    "<head>\n" +
+                    "<meta charset = 'UTF-8'>\n" +
+                    "<title>Fracture</title>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                    "</body>\n" +
+                    "</html>"
+            };
+        }
+    }
+}
